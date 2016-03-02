a9d925b53b00358e1d298a71e7ee7e0a4a5d585d
Alex Waite <Alexqw85@gmail.com>
2016-03-02 14:17:50 +0100

    Cleanup pelicanconf and publishconf

--- src/pelicanconf.py
+++ src/pelicanconf.py
@@ -2,60 +2,61 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+#
+# About the site
+#
 AUTHOR = u'Michael Hanke'
 SITENAME = u'studyforrest.org'
 SITESUBTITLE = u'A de-centralized mass-collaboration attempt'
 SITEURL = ''
 
 TIMEZONE = 'Europe/Berlin'
-
 DEFAULT_LANG = u'en'
 LOCALE = u'en_US.UTF-8'
 
+#
+# Configure Pelican a bit
+#
+PLUGIN_PATHS = ['../pelican-plugins']
+PLUGINS = ['twitter_bootstrap_rst_directives', 'bootstrapify', 'tipue_search', 'pelican_javascript']
+
 THEME = '../pelican-theme'
+DIRECT_TEMPLATES = ['index', 'categories', 'authors', 'archives', 'search'] # unset all templates; use these
 
-DIRECT_TEMPLATES = ['index', 'categories', 'authors', 'archives', 'search']
-
-# Feed generation is usually not desired when developing
+#
+# Disable when developing
+#
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
+#
+# Configure the site
+#
+STATIC_PATHS = ['images', 'pics', 'css', 'js', 'data']
 LINKS = (('Psychoinformatics', 'http://psychoinformatics.de'),
          ('Real-Life Cognition Channel', 'http://f1000research.com/channels/real-cognition'),
          ('NeuroDebian', 'http://neuro.debian.net'),
          ('PyMVPA', 'http://www.pymvpa.org'))
+DEFAULT_PAGINATION = 10
 
-# Social widget
+#
+# Social media jazz
+#
 SOCIAL = (('twitter', 'http://twitter.com/studyforrest'),
           ('github', 'http://github.com/hanke/gumpdata'),)
 
-DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = False
-
-STATIC_PATHS = ['images', 'pics', 'css', 'js', 'data']
-
-PLUGIN_PATHS = ['../pelican-plugins']
-PLUGINS = ['twitter_bootstrap_rst_directives', 'bootstrapify', 'tipue_search',
-           'pelican_javascript']
-
-DISPLAY_TAGS_INLINE = True
-DISPLAY_ARTICLE_INFO_ON_INDEX = True
-
 ADDTHIS_PROFILE = 'ra-53a573780cc42d73'
 ADDTHIS_DATA_TRACK_ADDRESSBAR = False
-
-RESPONSIVE_IMAGES = True
-
 GOOGLE_ANALYTICS = "UA-46839658-1"
-
-
 TWITTER_USERNAME = 'studyforrest'
 TWITTER_WIDGET_ID = '435327568237965312'
 
+#
+# Likely to be removed
+#
+RESPONSIVE_IMAGES = True
+DISPLAY_TAGS_INLINE = True
+DISPLAY_ARTICLE_INFO_ON_INDEX = True
 WITH_FUTURE_DATES = True
-
 SHOW_ARTICLE_AUTHOR = True
--- src/publishconf.py
+++ src/publishconf.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
+# This file is only used when publishing the site (`make publish/rsync_upload/etc`)
 
 import os
 import sys
@@ -15,5 +14,6 @@ RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+TAG_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
