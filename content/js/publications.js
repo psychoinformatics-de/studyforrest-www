8017afa9fb5c49ce0b1d4a42e0a0072a74a0933c
Alex Waite <Alexqw85@gmail.com>
2016-05-31 16:29:12 +0200

    Add filtering (via URL param) to publications table

--- content/js/publications.js
+++ content/js/publications.js
@@ -1,3 +1,36 @@
+// :icontains - a case insensitive jQuery selector
+// https://stackoverflow.com/a/12113443
+jQuery.expr[":"].icontains = jQuery.expr.createPseudo(function (arg) {
+  return function (elem) {
+    return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+  };
+});
+
+
+function getURLParam(param) {
+  var val = '';
+  location.search.substr(1)
+    .split("&")
+    .some(function(item) {
+      return item.split("=")[0] == param && (val = item.split("=")[1])
+    });
+  return val;
+}
+
+
+function filterTable(filter) {
+  $('tbody > tr').show();
+
+  if (filter && filter.trim().length) {
+    $('tbody > tr:not(:icontains(' + filter + '))').hide();
+  }
+}
+
+
 $(document).ready(function() {
-    $("table").tablesorter();
+  $("table").tablesorter();
+
+  var filter = getURLParam('filter');
+
+  filterTable(filter);
 });
