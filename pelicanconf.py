5d72b0b483f372eae8d261e07c94124f34a60d5e
Alex Waite <alex@waite.eu>
2021-04-23 15:56:08 +0200

    simplify and uplift pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,32 +5,30 @@ from __future__ import unicode_literals
 #
 # About the site
 #
-AUTHOR = u'Michael Hanke & Alex Waite'
-SITENAME = u'studyforrest.org'
+AUTHOR = 'Michael Hanke & Alex Waite'
+SITENAME = 'studyforrest.org'
 SITEURL = ''
 
 TIMEZONE = 'Europe/Berlin'
-DEFAULT_LANG = u'en'
-LOCALE = u'en_US.UTF-8'
+DEFAULT_LANG = 'en'
+LOCALE = 'en_US.UTF-8'
 
 #
 # Configure Pelican a bit
 #
+PATH = 'content'
 PLUGIN_PATHS = ['pelican-plugins']
 PLUGINS = ['better_tables', 'bootstrap-rst', 'pelican_javascript', 'sitemap', 'tipue_search',]
 SITEMAP = { 'format': 'xml' }
 
 THEME = 'theme'
 DIRECT_TEMPLATES = ['archives', 'search'] # unset all templates; use these
+STATIC_PATHS = ['img/']
+
+#HEADERID_LINK_CHAR = '<i class="icon-link"></i>'
 
 FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
 AUTHOR_SAVE_AS = False
-CATEGORY_SAVE_AS = False
 
 #
 # Configure the site
