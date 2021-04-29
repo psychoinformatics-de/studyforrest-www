2e29444c01df5db576de11df5096df1437ae0389
Alex Waite <alex@waite.eu>
2021-04-29 11:34:48 +0200

    fix favicons; copy static/ correctly
    This was broken with the move away from the Makefile (where we simply
    used rsync).
    
    STATIC_PATHS only searches in Pelican's PATH, which is set to content/.
    Escaping the PATH using ../ does not make pelican happy, and causes it
    to throw warnings everywhere and attempt overwriting its own static
    files.
    
    There is no way I can find of to copy an entire folder into the top-level
    of output.
    
    STATIC_PATHS only accepts dirs, and EXTRA_PATH_METADATA does not accept
    patterns or wildcards for the key.
    
    So this is the solution I've come up with: specifying each file by hand.
    
    Sigh...

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,7 +23,23 @@ SITEMAP = { 'format': 'xml' }
 
 THEME = 'theme'
 DIRECT_TEMPLATES = ['search'] # unset all templates; use these
-STATIC_PATHS = ['img/']
+STATIC_PATHS = ['img/', 'static/']
+EXTRA_PATH_METADATA = {
+    "static/android-chrome-192x192.png": {'path': ''},
+    "static/android-chrome-512x512.png": {'path': ''},
+    "static/apple-touch-icon.png": {'path': ''},
+    "static/browserconfig.xml": {'path': ''},
+    "static/favicon-16x16.png": {'path': ''},
+    "static/favicon-32x32.png": {'path': ''},
+    "static/favicon.ico": {'path': ''},
+    "static/mstile-144x144.png": {'path': ''},
+    "static/mstile-150x150.png": {'path': ''},
+    "static/mstile-310x150.png": {'path': ''},
+    "static/mstile-310x310.png": {'path': ''},
+    "static/mstile-70x70.png": {'path': ''},
+    "static/safari-pinned-tab.svg": {'path': ''},
+    "static/site.webmanifest": {'path': ''},
+}
 
 #HEADERID_LINK_CHAR = '<i class="icon-link"></i>'
 
@@ -33,7 +49,6 @@ AUTHOR_SAVE_AS = False
 #
 # Configure the site
 #
-STATIC_PATHS = ['img']
 MENUITEMS = ( ('About', 'about.html'),
               ('Access', 'access.html'),
               ('Data', 'data.html'),
